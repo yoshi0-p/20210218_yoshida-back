fbb472977d806b441cf9a3677e861ddbd45c57ba
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-02-18 07:22:11 +0900

    message

--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'paths' => ['*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_methods' => ['https://affectionate-sinoussi-89570e.netlify.app/'],
+    'allowed_origins' => ['https://affectionate-sinoussi-89570e.netlify.app/'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['https://affectionate-sinoussi-89570e.netlify.app/'],
     'exposed_headers' => false,
     'max_age' => false,
     'supports_credentials' => false,
