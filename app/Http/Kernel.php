eb370c1c4f2df9a4a3d42b13ab0a2d1729e775be
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-02-21 08:52:40 +0900

    message

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,10 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'cors'          => \App\Http\Middleware\Cors::class,
+        'api' => [
+            'throttle:60,1',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Cors::class, // ★追加
+        ],
     ];
 }
