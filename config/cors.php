c9725d31c4ff6be2c7884ff8f03caf2c1046440a
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-02-21 08:31:23 +0900

    message

--- config/cors.php
+++ config/cors.php
@@ -1,36 +1,13 @@
 <?php
 
-namespace App\Http\Middleware;
 return [
-'paths' => ['*'],
-'allowed_methods' => ['*'],
-'allowed_origins' => ['*'],
-'allowed_origins_patterns' => [],
-'allowed_headers' => ['*'],
-'exposed_headers' => false,
-'max_age' => false,
-'supports_credentials' => false,
+  'paths' => ['*'],
+  'allowed_methods' => ['*'],
+  'allowed_origins' => ['*'],
+  'allowed_origins_patterns' => [],
+  'allowed_headers' => ['*'],
+  'exposed_headers' => false,
+  'max_age' => false,
+  'supports_credentials' => false,
+  'Access-Control-Allow-Origin' => ['*'],
 ];
-
-
-
-
-use Closure;
-
-class Cors
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:8080')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type');
-    }
-}
