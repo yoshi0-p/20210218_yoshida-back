11e3311ea39257eabd59cd19983f47fa934ceb72
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-02-21 08:23:50 +0900

    message

--- config/cors.php
+++ config/cors.php
@@ -1,4 +1,6 @@
 <?php
+
+namespace App\Http\Middleware;
 return [
 'paths' => ['*'],
 'allowed_methods' => ['*'],
@@ -9,3 +11,26 @@ return [
 'max_age' => false,
 'supports_credentials' => false,
 ];
+
+
+
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type');
+    }
+}
